251c0e57bb31196b195e515215f27b453e4ff4af
JanekBancarewicz <jan.bancarkiewicz@rb.com>
2021-01-24 15:17:59 +0100

    first commit

---
+++ src/pages/projects.js
@@ -0,0 +1,45 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { graphql } from "gatsby"
+import Projects from "../components/Projects"
+
+const ProjectsPage = ({
+  data: {
+    allStrapiProjects: { nodes: projects },
+  },
+}) => {
+  return (
+    <Layout>
+      <section className="projects-page">
+        <Projects projects={projects} title="all projects"></Projects>
+      </section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        github
+        id
+        description
+        title
+        url
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        stack {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+export default ProjectsPage
