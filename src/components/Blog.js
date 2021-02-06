dddbb26cfd1e69ffcd65cc55eb7fd67b0adb7e0e
JanekBancarewicz <jan.bancarkiewicz@rb.com>
2021-02-06 15:49:05 +0100

    layout

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,9 +4,17 @@ import Image from "gatsby-image"
 import { Link } from "gatsby"
 const Blog = ({ id, title, slug, image, date, category, desc }) => {
   return (
-    <Link to={`/blogs/${slug}`} key={id}>
+    <Link to={`/blogs/${slug}`} className="blog" key={id}>
       <article>
         <Image fluid={image.childImageSharp.fluid} className="blog-img" />
+        <div className="blog-card">
+          <h4>{title}</h4>
+          <p>{desc}</p>
+          <div className="blog-footer">
+            <p>{category}</p>
+            <p>{date}</p>
+          </div>
+        </div>
       </article>
     </Link>
   )
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,10 +2,35 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Blogs from "../components/Blogs"
-// ...GatsbyImageSharpFluid
 
-const Blog = () => {
-  return <h2>blog page</h2>
+const Blog = (props) => {
+  return (
+    <Layout>
+      <section className="blog-page">
+        <Blogs blogs={props.data?.allStrapiBlogs.nodes} title="blog" />
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiBlogs {
+      nodes {
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        date(formatString: "MMMM Do, YYYY")
+        id
+        slug
+        desc
+      }
+    }
+  }
+`
 
 export default Blog
